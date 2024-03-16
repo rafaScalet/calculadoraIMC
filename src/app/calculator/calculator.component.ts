5bbce8e8b1c0c4f4caa5ca2921f293378d454986
rafaScalet <rafaelscalet2018@gmail.com>
2024-03-16 11:40:14 -0300

    added the images and more improvements on design

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -11,25 +11,31 @@ export class CalculatorComponent {
   result: number = 0;
   state: string = "";
   aux: boolean = false;
-  txt: string = ""
+  txt: string = "insira os valores acima";
+  img: string = ""
 
   calc(){
     this.result = this.peso / (this.altura)**2;
 
     if(this.result <= 18.5){
       this.state = "magreza";
+      this.img = "assets/magreza.png";
     }
     else if(this.result <= 24.9){
-      this.state = "uma pessoa normal"
+      this.state = "uma pessoa normal";
+      this.img = "assets/normal.png";
     }
     else if(this.result <= 29.9){
-      this.state = "sobrepeso"
+      this.state = "sobrepeso";
+      this.img = "assets/sobrepeso.png";
     }
     else if(this.result <= 39.9){
       this.state = "obesidade"
+      this.img = "assets/obesidade.png";
     }
     else if(this.result >= 40){
       this.state = "obesidade grave"
+      this.img = "assets/obesidade_grave.png";
     }
     this.aux = !this.aux;
 
